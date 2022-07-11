a948fd8cd6394d7e612684972a19cd88120b2071
Karen <karen.sn.amb@gmail.com>
2022-07-11 17:38:22 -0300

    commitando iten_obrigatorios/portas

--- itens_obrigatorios/portas.java
+++ itens_obrigatorios/portas.java
@@ -1,4 +1,35 @@
 package itens_obrigatorios;
 
-public class portas {
+public class Portas {
+    private Integer quantidadePortas;
+    private String tipoVidro;
+
+    public Portas(String tipoVidro, Integer quantidadePortas){
+        this.quantidadePortas= quantidadePortas;
+        this.tipoVidro= tipoVidro;
+
+    }
+
+    public Integer getQuantidadePortas() {
+
+        return quantidadePortas;
+    }
+
+    public void setQuantidadePortas(Integer quantidadePortas) {
+
+        this.quantidadePortas = quantidadePortas;
+    }
+
+    public String getTipoVidro() {
+
+        return tipoVidro;
+    }
+
+    public void setTipoVidro(String tipoVidro) {
+        this.tipoVidro = tipoVidro;
+
+
+    }
+
+
 }
